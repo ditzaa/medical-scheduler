cca24f8a10e168b87b2b4c4d7835845480fa734a
Mitu Adrian <adrianmitu83@gmail.com>
2025-12-28 14:11:16 +0200

    Initial project

---
+++ medical-scheduler/src/main/java/com/medisched/scheduler/MedicalSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.medisched.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalSchedulerApplication.class, args);
+	}
+
+}
---
+++ medical-scheduler/src/test/java/com/medisched/scheduler/MedicalSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisched.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
